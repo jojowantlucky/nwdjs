dcdbec5d3b59a3a80c384eeeaee73a14eeb8a33e
Joe <joe@joeebner.com>
2026-03-03 00:54:14 +0700

    BookWithConfidence: update WeddingWire badge to 2025 Couples Choice

--- src/components/ui/WeddingWireRatedBadge.tsx
+++ src/components/ui/WeddingWireRatedBadge.tsx
@@ -4,33 +4,40 @@ import { useEffect } from 'react'
 
 export default function WeddingWireRatedBadge() {
   useEffect(() => {
-    // Load the WeddingWire rated script if not already present
     if (!document.querySelector('script[src*="wp-rated.js"]')) {
       const script = document.createElement('script')
       script.src = 'https://cdn1.weddingwire.com/_js/wp-rated.js?v=4'
       script.async = true
       script.onload = () => {
-        if (typeof (window as any).wpShowRatedWW === 'function') {
-          (window as any).wpShowRatedWW('647219')
+        if (typeof (window as any).wpShowRatedWAv3 === 'function') {
+          (window as any).wpShowRatedWAv3('647219', '2025')
         }
       }
       document.head.appendChild(script)
-    } else if (typeof (window as any).wpShowRatedWW === 'function') {
-      (window as any).wpShowRatedWW('647219')
+    } else if (typeof (window as any).wpShowRatedWAv3 === 'function') {
+      (window as any).wpShowRatedWAv3('647219', '2025')
     }
   }, [])
 
   return (
-    <div style={{ width: '150px', height: '150px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+    <div
+      id="wp-ratedWA"
+      style={{ width: '150px', height: '150px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+    >
       <a
-        id="wp-rated-img"
         target="_blank"
-        rel="nofollow noreferrer"
         href="https://www.weddingwire.com/biz/noteworthy-djs-portland/9661cffdf7751c14.html"
-        title="Reviewed on WeddingWire"
-        style={{ display: 'block', textAlign: 'center' }}
+        rel="nofollow noreferrer"
+        title="WeddingWire Couples' Choice Award Winner 2025"
       >
-        <span id="wp-rated-reviews" />
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          id="wp-ratedWA-img-2025"
+          width={125}
+          height={125}
+          alt="Noteworthy DJs & Photo Booths"
+          src="https://cdn1.weddingwire.com/img/badges/2025/badge-weddingawards_en_US.png"
+        />
       </a>
     </div>
   )
