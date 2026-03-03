218dfd2c3433fb7265cced987380ea60475ae7b4
Joe <joe@joeebner.com>
2026-03-03 16:24:56 +0700

    Photo booth: force-cache with .next clear on build to bust stale API cache

--- out/_next/static/chunks/app/song-lists/[slug]/page-42c04c1612852406.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[201,828,980,898,902,585],{2822:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=2822)}),_N_E=n.O()}]);
